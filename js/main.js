66e8ea6d8a193b6fa563cde2e6f6b668a2e02f66
Carter G <carterguthrie94@yahoo.com>
2014-11-17 21:14:28 -0500

    updated demo, java now

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+
+element = tag = Node
+
+NODE=
+Element - type 1
+ATTRIBUTE - type 2
+TEXT - type 3
+
+*/
+
+
+document.getElementById('main')
+document.querySelector('.active');
+document.querySelectorAll('.active');
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+document.getElementById("main"),document.querySelector(".active"),document.querySelectorAll(".active");
