ab898902ba40a60c485722369f6c6aa5ebb314ec
Carter G <carterguthrie94@yahoo.com>
2014-11-17 21:37:49 -0500

    updated demo, alerts java

--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@
 element = tag = Node
 
 NODE=
-Element - type 1
+Element - type 1 
 ATTRIBUTE - type 2
 TEXT - type 3
 
-*/
 
 
 document.getElementById('main')
 document.querySelector('.active');
 document.querySelectorAll('.active');
+*/
+
+
+var columns = document.querySelectorAll('.columns')
+
+for(var item in columns){
+	columns[item].onclick = function(){
+	this.style.visibility = "hidden";
+	}
+}
--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,25 @@
-document.getElementById("main"),document.querySelector(".active"),document.querySelectorAll(".active");
+/*
+
+element = tag = Node
+
+NODE=
+Element - type 1 
+ATTRIBUTE - type 2
+TEXT - type 3
+
+
+
+document.getElementById('main')
+document.querySelector('.active');
+document.querySelectorAll('.active');
+*/
+
+
+var columns = document.querySelectorAll('.columns')
+
+for(var item in columns){
+	columns[item].onclick = function(){
+	this.style.visibility = "hidden";
+	}
+}
+
